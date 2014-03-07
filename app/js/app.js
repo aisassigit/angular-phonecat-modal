72fde8760ba2bb00be0b16efff85f1d308f69976
aisassigit <adolfo.isassi@gmail.com>
2014-03-06 21:04:01 -0700

    -Added location change event based breadcrumb navigation

--- app/js/app.js
+++ app/js/app.js
@@ -9,7 +9,8 @@ var phonecatApp = angular.module('phonecatApp', [
   'phonecatControllers',
   'phonecatFilters',
   'phonecatServices',
-  'services.breadcrumbs'
+  'services.breadcrumbs',
+  'ui.router'
 ]);
 
 phonecatApp.config(['$routeProvider',
@@ -31,6 +32,28 @@ phonecatApp.config(['$routeProvider',
         redirectTo: '/phones'
       });
   }]);
+
+//phonecatApp.config(function($stateProvider, $urlRouterProvider) {
+//  // For any unmatched url, redirect to /state1
+//  $urlRouterProvider.otherwise("/phones");
+//  // Now set up the states
+//  $stateProvider
+//    .state('phones', {
+//      url: "/phones",
+//      templateUrl: "partials/phone-list.html",
+//      controller:'PhoneListCtrl'
+//    })
+//    .state('phone', {
+//      url: "/phones/:phoneId",
+//      templateUrl: "partials/phone-detail.html",
+//      controller: 'PhoneDetailCtrl'
+//    })
+//    .state('modal', {
+//      url: "/phones/modal/:phoneId",
+//      templateUrl: "partials/modal-detail.html",
+//      controller:'PhoneModalDetailCtrl'
+//    })
+//});
 
 phonecatApp.controller('AppCtrl', ['$scope','breadcrumbs','$location', function($scope,breadcrumbs,$location) {
     $scope.location = $location;
